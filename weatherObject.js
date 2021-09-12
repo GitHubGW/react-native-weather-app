fa5ffdb9884538153ba0d5cb25eb1bc12f750820
GW <kowonp@gmail.com>
2021-09-12 19:57:35 +0900

    Created weather object

---
+++ weatherObject.js
@@ -0,0 +1,84 @@
+const weatherObject = {
+  Clear: {
+    title: "맑음",
+    iconName: "weather-sunny",
+    gradient: ["#c2e59c", "#64b3f4"],
+  },
+  Clouds: {
+    title: "흐림",
+    iconName: "cloud",
+    gradient: ["#283048", "#859398"],
+  },
+  Thunderstorm: {
+    title: "천둥 번개",
+    iconName: "weather-lightning",
+    gradient: ["#009FFF", "#ec2F4B"],
+  },
+  Drizzle: {
+    title: "이슬비",
+    iconName: "weather-rainy",
+    gradient: ["#7F7FD5", "#91EAE4"],
+  },
+  Rain: {
+    title: "비",
+    iconName: "weather-pouring",
+    gradient: ["#24C6DC", "#514A9D"],
+  },
+  Snow: {
+    title: "눈",
+    iconName: "weather-snowy",
+    gradient: ["#E0EAFC", "#CFDEF3"],
+  },
+  Atmosphere: {
+    title: "구름 조금",
+    iconName: "weather-sunny",
+    gradient: ["#FFEEEE", "#DDEFBB"],
+  },
+  Mist: {
+    title: "안개",
+    iconName: "weather-partly-cloudy",
+    gradient: ["#8e9eab", "#eef2f3"],
+  },
+  Smoke: {
+    title: "구름 많음",
+    iconName: "weather-cloudy",
+    gradient: ["#606c88", "#A0A0A0"],
+  },
+  Haze: {
+    title: "안개",
+    iconName: "weather-hazy",
+    gradient: ["#bdc3c7", "#2c3e50"],
+  },
+  Dust: {
+    title: "황사",
+    iconName: "weather-cloudy",
+    gradient: ["#1e130c", "#9a8478"],
+  },
+  Fog: {
+    title: "안개",
+    iconName: "weather-fog",
+    gradient: ["#616161", "#9bc5c3"],
+  },
+  Sand: {
+    title: "황사",
+    iconName: "weather-cloudy",
+    gradient: ["#403B4A", "#E7E9BB"],
+  },
+  Ash: {
+    title: "황사",
+    iconName: "weather-cloudy",
+    gradient: ["#16222A", "#3A6073"],
+  },
+  Squall: {
+    title: "돌풍",
+    iconName: "weather-windy",
+    gradient: ["#16222A", "#3A6073"],
+  },
+  Tornado: {
+    title: "태풍",
+    iconName: "weather-tornado",
+    gradient: ["#780206", "#061161"],
+  },
+};
+
+export default weatherObject;
